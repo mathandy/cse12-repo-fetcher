d90ed8ad3353185a4c3f2f5c2b8e60f458c39e3f
Andrew Port <andyaport@gmail.com>
2020-12-15 00:46:30 -0800

    add script to create moss dir (must run manually)

--- create-moss-dir.sh
+++ create-moss-dir.sh
@@ -1,4 +1,9 @@
 n=5
 mkdir ~/lab${n}s
-ls -d */ > ../lab${n}s
+ls -d * > ../lab${n}s
 for x in $(cat ../lab${n}s); cp $x/lab${n}/lab${n}.asm ~/lab${n}s/$x.asm
+
+# cd into spring repos and run
+n=5
+ls -d * > ../lab${n}s
+for x in $(cat ../lab${n}s); cp $x/lab${n}/lab${n}.asm ~/lab${n}s/SPRING_2020/$x.asm
