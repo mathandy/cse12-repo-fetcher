4b7dd46977f26ce67814c6e937174d8f1eeecc28
Andrew Port <andyaport@gmail.com>
2021-05-25 00:33:29 -0700

    updated copy/paste script

--- create-moss-dir.sh
+++ create-moss-dir.sh
@@ -4,7 +4,8 @@
 exit
 
 n=3
-OUTDIR=~/lab${n}s
+term="s21"
+OUTDIR="~/lab${n}s-$term"
 mkdir $OUTDIR
 cd repos | echo "ERROR" & exit
 ls -d * > ../lab${n}s.txt
@@ -12,10 +13,11 @@ for x in $(cat ../lab${n}s.txt); cp $x/lab${n}/lab${n}.asm "$OUTDIR"/$x.asm
 
 # cd into spring repos and run
 n=3
-OUTDIR=~/lab${n}s
+term="s21"
+OUTDIR="~/lab${n}s-$term"
 cd repos
 ls -d * > ../lab${n}s
-for x in $(cat ../lab${n}s); cp $x/lab${n}/lab${n}.asm ~/lab${n}s/SPRING_2020/$x.asm
+for x in $(cat ../lab${n}s); cp $x/lab${n}/lab${n}.asm "$OUTDIR"/SPRING_2020/$x.asm
 
 perl ~/moss.pl -l mips {F20/*,*.asm}
 perl ~/moss.pl -l mips *.asm
